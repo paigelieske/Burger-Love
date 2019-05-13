9b96ba2fff2d5a3c0b2c0c6446cf11ac4c5bfa40
Paige Lieske <paigeproctor@comcast.net>
2019-05-13 18:11:34 -0400

    added css floats

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE burgers_table (
 	id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN default FALSE,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
     );
